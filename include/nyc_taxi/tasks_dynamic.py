d94618b7078bd8be516b57a46f148a5129acc1b6
Niral Bharat <niral@Nirals-MBP.Home>
2026-01-23 11:32:44 +0000

    completed first stage quality checks

--- include/nyc_taxi/tasks_dynamic.py
+++ include/nyc_taxi/tasks_dynamic.py
@@ -24,13 +24,17 @@ def generate_monthly_dates_2024():
 
 def run_data_quality_checks(year_month, 
                             min_rows=10000, 
-                            max_null_pct=1.0, 
-                            max_neg_duration_pct=0.1):
+                            max_col_null_pct=0.01, 
+                            max_total_null_pct = 0.01,
+                            max_neg_duration_pct=0.01):
     """
-    Simple quality checks on raw Parquet taxi files in S3:
-    - no null pickup/dropoff timestamps
-    - non-negative trip duration (in seconds)
+    Simple quality checks on raw Parquet files in S3:
+    - required columns are present and in expected data types.
+    - row count exceeds minimum row threshold.
+    - no null pickup/dropoff timestamps.
+    - non-negative trip durations below specified threshold
     - file schema validation
+    - column and total null percentages are within specified threshold.
     """
     
     # check each available month has a file present in s3
@@ -45,8 +49,6 @@ def run_data_quality_checks(year_month,
     key = s3_fs.ls(f"s3://{s3_key}")
     if not key:
         raise errors.DataSourceMissingError(f"No Parquet files found under s3://{s3_key}")
-
-    error_messages = []
 
     # Build full S3 URL as fs.open requires path to be "s3://bucket/key"
     storage_options = {
@@ -72,7 +74,7 @@ def run_data_quality_checks(year_month,
     dtype_mapping = {
         'tpep_pickup_datetime': 'datetime64[ns]',
         'tpep_dropoff_datetime': 'datetime64[ns]',
-        'passenger_count': 'int32',
+        'passenger_count': 'float64',
         'trip_distance': 'float32',
         'fare_amount': 'float32',
         'total_amount': 'float32',
@@ -86,7 +88,7 @@ def run_data_quality_checks(year_month,
             try:
                 df[col] = df[col].astype(dtype)
             except:
-                dtype_errors.append(f"Schema error: column '{col}' has dtype '{dtype}', expected '{dtype_mapping[col]}'")
+                dtype_errors.append(f"Schema error: column '{col}' has dtype '{df[col].dtype}', expected '{dtype_mapping[col]}'")
     
     if dtype_errors:     
         msg = '\n'.join(dtype_errors)
@@ -97,42 +99,50 @@ def run_data_quality_checks(year_month,
     if file_row_count < min_rows:
         raise errors.MiniumumRowsError(file_row_count, min_rows)
 
+
     pickup_col = "tpep_pickup_datetime"
     dropoff_col = "tpep_dropoff_datetime"
 
-    # check columns are in datetime format, non conforming entries set to na.
+    # convert columns to datetime format, non conforming entries set to na to give correct null count
     df[pickup_col] = pd.to_datetime(df[pickup_col], errors="coerce")
     df[dropoff_col] = pd.to_datetime(df[dropoff_col], errors="coerce")
+
+    pickup_null_count = df[pickup_col].isna().sum()
+    dropoff_null_count = df[dropoff_col].isna().sum()
+    
+    if pickup_null_count > 0:
+        raise errors.NonNullColumnError(col=pickup_col, null_count=pickup_null_count)
+    if dropoff_null_count > 0:
+        raise errors.NonNullColumnError(col=dropoff_col, null_count=dropoff_null_count)
 
     # create new col "trip duration" for trip duration in minutes
     df["trip_duration_seconds"] = (df[dropoff_col] - df[pickup_col]).dt.total_seconds() / 60
 
-    # null value count across all required columns
-    req_cols_null_count = len(df[df["tpep_pickup_datetime"].isna() | 
-                                       df["tpep_dropoff_datetime"].isna() |
-                                       df["passenger_count"].isna() |
-                                       df["trip_distance"].isna() |
-                                       df["fare_amount"].isna() |
-                                       df["total_amount"].isna() |
-                                       df["PULocationID"].isna() |
-                                       df["DOLocationID"].isna() 
-                                       ])
+    # check each column's null percentage is below specified limit
+    # check total null percentage is below specified limit
+    req_cols_total_null_count = 0
+    null_error_cols = []
+    for col in required_columns:
+        null_count = df[col].isna().sum() 
+        null_pct = null_count / file_row_count if file_row_count > 0 else 0
+        req_cols_total_null_count += null_count
+        if null_pct > max_col_null_pct:
+            null_error_cols.append(null_error_cols)
+    
+    if null_error_cols:
+        raise errors.NullThresholdError(cols=null_error_cols, threshold_pct=max_col_null_pct)
+    if file_row_count > 0:
+        total_null_pct = req_cols_total_null_count / file_row_count
+        if total_null_pct > max_total_null_pct:
+            raise errors.TotalNullsThresholdError(total_null_threshold_pct=max_total_null_pct, total_null_pct=total_null_pct)
     
     #check for negative duration values
     neg_duration_count = len(df[df["trip_duration_seconds"] < 0])   
-
-    # calculate percentages
-    req_cols_null_pct =  req_cols_null_count / file_row_count if file_row_count else 0
     neg_duration_pct = neg_duration_count / file_row_count if file_row_count else 0
 
-    # raise error if thresholds exceeded
-    if req_cols_null_pct > max_null_pct:
-        raise errors.NullThresholdError(req_cols_null_count, max_null_pct)
+    # raise error if neg duration threshold exceeded
     if neg_duration_pct > max_neg_duration_pct:
         raise errors.NegativeDurationThresholdError(neg_duration_pct, max_neg_duration_pct)
-    
-    for msg in error_messages:
-        print(f"❌ {msg}")
 
     return s3_key
 
